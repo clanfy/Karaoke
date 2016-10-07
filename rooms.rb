00177c8a03f458ef685e16d04bcc11848151a292
Clacson <cookiellacson@gmail.com>
2016-10-07 13:39:48 +0100

    test_room_has_name and test_5_rooms passed

--- rooms.rb
+++ rooms.rb
@@ -1,3 +1,11 @@
-def initialize
+class Room
+
+  attr_reader :name, :song_list
+
+def initialize(name, song_list)
+  @name = name
+  @song_list = song_list
+
+end
 
 end
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -3,23 +3,30 @@ require('minitest/rg')
 require('pry-byebug')
 require_relative('../rooms')
 require_relative('../songs')
-# require_relative('../guests')
+
 
 class TestRoom < MiniTest::Test
 
   def setup
 
-    @room_1 = Room.new("Scary", song_list)
-    @room_2 = Room.new("Ginger", song_list)
-    @room_3 = Room.new("Baby", song_list)
-    @room_4 = Room.new("Sporty", song_list)
-    @room_5 = Room.new("Posh", song_list)
+    @room_1 = Room.new("Scary", @song_list)
+    @room_2 = Room.new("Ginger", @song_list)
+    @room_3 = Room.new("Baby", @song_list)
+    @room_4 = Room.new("Sporty", @song_list)
+    @room_5 = Room.new("Posh", @song_list)
+
+    @rooms = [@room_1, @room_2, @room_3, @room_4, @room_5]
 
   end
 
 def test_room_has_name
-
+  assert_equal("Posh", @room_5.name)
 end
+
+def test_5_rooms
+  assert_equal(5, @rooms.count)
+end
+
 
 
 end
