e057af2e4e8996f74cba3e013139a18e42880a97
Clacson <cookiellacson@gmail.com>
2016-10-07 11:58:46 +0100

    Songs class set up and song_has_title test passed

--- specs/songs_spec.rb
+++ specs/songs_spec.rb
@@ -1,3 +1,15 @@
 require('minitest/autorun')
-require('mminitest/rg')
+require('minitest/rg')
 require_relative('../songs')
+
+class TestSongs < Minitest::Test
+
+  def setup
+    @song_1 = Song.new("The River", "Bruce Springsteen")
+  end
+
+  def test_song_has_title
+    assert_equal("The River", @song_1.title)
+  end
+
+end
